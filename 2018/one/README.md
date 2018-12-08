b17b42104f2c6031d6869c18b8add925ab22763f
Dan Reeves <hey@danreev.es>
2018-12-08 17:54:55 +0000

    2018 01 02

--- 2018/one/README.md
+++ 2018/one/README.md
@@ -1,5 +1,16 @@
 # Use
 
+## Task one:
+
 ```sh
-cargo run input.txt
+cargo run 1 input.txt
+Result: 486
+```
+
+## Task two:
+
+```sh
+cargo run 2 input.txt
+Took 143 iterations
+Result: 69285
 ```
--- 2018/one/src/main.rs
+++ 2018/one/src/main.rs
@@ -1,16 +1,68 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
 
 fn main() {
-    let args = env::args();
-    if args.len() != 2 {
-        println!("Usage: one filename.txt");
+    let mut args = env::args();
+    if args.len() != 3 {
+        println!("Usage: one [1|2] [filename]");
         return;
     }
-    let filename = args.last().unwrap();
-    let contents = fs::read_to_string(filename).unwrap();
-    let input = contents.split('\n').map(|line| line.parse::<i64>().unwrap_or(0)).collect::<Vec<i64>>();
 
+    args.next();
+
+    let task = args.next().unwrap();
+    let filename = args.next().unwrap();
+    let contents = fs::read_to_string(filename).unwrap();
+    let input = contents
+        .split('\n')
+        .filter(|line| !line.is_empty())
+        .map(|line| line.parse::<i64>().unwrap_or(0))
+        .collect::<Vec<i64>>();
+
+    let output = match task.as_ref() {
+        "1" => task_1(input),
+        "2" => task_2(input),
+        _ => String::from(""),
+    };
+
+    println!("Result: {}", output);
+}
+
+fn task_1(input: Vec<i64>) -> String {
     let output = input.iter().fold(0, |acc, x| acc + x);
-    println!("{}", output);
+    format!("{}", output)
+}
+
+fn task_2(input: Vec<i64>) -> String {
+    let mut double_input = input.clone();
+    double_input.append(input.clone().as_mut());
+
+    let mut acc = 0;
+    let mut known_values: HashMap<i64, ()> = HashMap::new();
+    let mut output: Option<i64> = None;
+    let mut iterations = 0;
+
+    while output.is_none() {
+        iterations = iterations + 1;
+        output = acc_til_dupe(&mut acc, &mut known_values, &input);
+    }
+
+    println!("Took {} iterations", iterations);
+    format!("{}", output.unwrap())
+}
+
+fn acc_til_dupe(
+    acc: &mut i64,
+    known_values: &mut HashMap<i64, ()>,
+    input: &Vec<i64>,
+) -> Option<i64> {
+    for i in input {
+        *acc = *acc + i;
+        let oldvalue = known_values.insert(*acc, ());
+        if oldvalue.is_some() {
+            return Some(acc.clone());
+        }
+    }
+    None
 }
