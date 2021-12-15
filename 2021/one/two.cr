0dfe69575f402cf5edac3d91c1ec9696f2402edb
Dan Reeves <hey@danreev.es>
2021-12-15 18:58:27 +0000

    some days

--- 2021/one/two.cr
+++ 2021/one/two.cr
@@ -3,23 +3,3 @@ content = File.read("./input.txt")
 prev = nil
 curr = nil
 count = 0
-
-content.each_line.cons(3).each do |chunk|
-  curr = chunk.map do |s|
-    s.to_i
-  end
-  if prev && curr
-    a = prev.reduce do |acc, i|
-      acc + i
-    end
-    b = curr.reduce do |acc, i|
-      acc + i
-    end
-    if curr > prev
-      count = count + 1
-    end
-  end
-  prev = curr
-end
-
-puts count
