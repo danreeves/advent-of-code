c8b141fff81e4ba6cb5ed9973873a77b6d0bb034
Dan Reeves <hey@danreev.es>
2022-12-01 12:19:48 +0000

    start aoc 2022

---
+++ 2022/day-one.ts
@@ -0,0 +1,28 @@
+const textInput = Deno.readTextFileSync("./day-one-input");
+
+function sumAndSort(data: string): number[] {
+  return data.split("\n\n")
+    .map((elf) =>
+      elf
+        .split("\n")
+        .reduce((total, current) => total + parseInt(current, 10), 0)
+    ).sort((a, b) => b - a);
+}
+
+function mostCaloriesCarried(data: string) {
+  const highest = sumAndSort(data)[0];
+  console.log("Highest calories carried: ", highest);
+}
+
+mostCaloriesCarried(textInput);
+
+function totalTopThreeCaloriesCarried(data: string) {
+  const highest = sumAndSort(data).slice(0, 3);
+
+  console.log(
+    `Highest calories carried by 3: `,
+    highest.reduce((t, v) => t + v, 0),
+  );
+}
+
+totalTopThreeCaloriesCarried(textInput);
